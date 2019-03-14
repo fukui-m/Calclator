38152b093dbed9f722517babe9f88d0dcb9ab7ae
fukui-m <fukui-m@evoltech.co.jp>
2019-03-14 17:15:03 +0900

    四則演算に対応　

--- Calclator/Calclator/Form1.cs
+++ Calclator/Calclator/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Calclator
 {
-    public partial class Form1 : Form
+    public partial class Form1 :Form
     {
         public Form1()
         {
@@ -19,12 +19,34 @@ namespace Calclator
 
         string firstInputNumber = "";
         string secondInputNumber = "";
-        string inputOperator = "";     
+        string inputOperator = "";
+        double calcResult=0;
        
                
         private void ButtonEqual_Click(object sender, EventArgs e)
         {
-
+            if (secondInputNumber != "")
+            {
+                if (inputOperator == "+")
+                {
+                    calcResult = int.Parse(firstInputNumber) + int.Parse(secondInputNumber);
+                }
+                else if (inputOperator == "-")
+                {
+                    calcResult = int.Parse(firstInputNumber) - int.Parse(secondInputNumber);
+                }
+                else if (inputOperator == "*")
+                {
+                    calcResult = int.Parse(firstInputNumber) * int.Parse(secondInputNumber);
+                }
+                else if (inputOperator == "/")
+                {
+                    calcResult = int.Parse(firstInputNumber) / int.Parse(secondInputNumber);
+                }
+            }
+            calcWindow.Text = calcResult.ToString();
+            firstInputNumber = calcResult.ToString();
+            secondInputNumber = "";
         }               
 
         
@@ -32,6 +54,9 @@ namespace Calclator
         {
             calcWindow.Text = "";
             firstInputNumber = calcWindow.Text;
+            secondInputNumber = calcWindow.Text;
+            inputOperator = calcWindow.Text;
+            calcResult = 0;
         }
 
         private void ButtonDelete_Click(object sender, EventArgs e)
@@ -59,10 +84,32 @@ namespace Calclator
 
         private void ButtonOperator_Click(object sender, EventArgs e)
         {
+            
+            if (secondInputNumber != "")
+            {
+                if (inputOperator == "+")
+                {
+                    calcResult = int.Parse(firstInputNumber) + int.Parse(secondInputNumber);
+                }
+                else if (inputOperator == "-")
+                {
+                    calcResult = int.Parse(firstInputNumber) - int.Parse(secondInputNumber);
+                }
+                else if (inputOperator == "*")
+                {
+                    calcResult = int.Parse(firstInputNumber) * int.Parse(secondInputNumber);
+                }
+                else if (inputOperator == "/")
+                {
+                    calcResult = int.Parse(firstInputNumber) / int.Parse(secondInputNumber);
+                }                
+                calcWindow.Text = calcResult.ToString();
+                firstInputNumber = calcResult.ToString();
+            }
             Button ButtonOperator = (Button)sender;
             string text = ButtonOperator.Text;
             inputOperator = text;
-            calcWindow.Text = inputOperator;
+            secondInputNumber = "";
         }
     }
 }
